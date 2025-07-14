6ceb57bcfe9ecd7bf7989f1e2055c0c4ecd295b3
Ngoc Anh <ngocanh030110@gmail.com>
2025-07-14 10:15:32 +0700

    11 How React works behind the scenes

--- 07-use-popcorn/src/App.tsx
+++ 07-use-popcorn/src/App.tsx
@@ -148,29 +148,6 @@ const Box: React.FC<BoxProps> = ({ children }) => {
     )
 }
 
-// const WatchedBox = () => {
-
-//   const [watched, setWatched] = useState(tempWatchedData);
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2((open) => !open)}
-//       >
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-//           <WatchedSummary watched={watched} />
-//           <WatchedList watched={watched} />
-//         </>
-//       )}
-//     </div>
-//   )
-// }
-
 interface MovieListProps {
     movies: Movie[]
 }
